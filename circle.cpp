5b8d391ea26cf23055739694c782bc9d2ed85f89
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2021-02-01 17:32:36 -0500

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,211 @@
+#include "circle.h"
+
+
+void draw_circle(SDL_Renderer* renderer, float x, float y, float r, int nPoints)
+{
+    if (nPoints < 0) nPoints = 0;
+
+    std::vector<SDL_FPoint> points;
+
+    for (float theta = 0; theta < 2 * (float) M_PI; theta += 2 * (float) M_PI / nPoints)
+    {
+        points.push_back({r * cosf(theta) + x, r * sinf(theta) + y });
+    }
+
+    points.push_back({ r * cosf(0) + x, r * sinf(0) + y }); // Connect last point to first
+
+    SDL_RenderDrawLinesF(renderer, &points[0], (int) points.size()); 
+}
+
+
+Vec2d add_v(Vec2d a, Vec2d b)
+{
+    return Vec2d{ a.x + b.x, a.y + b.y };
+}
+
+
+Vec2d sub_v(Vec2d a, Vec2d b)
+{
+    return Vec2d{ a.x - b.x, a.y - b.y };
+}
+
+
+Vec2d mul_s(float a, Vec2d b)
+{
+    return Vec2d{ a * b.x, a * b.y };
+}
+
+float mag_sqr(Vec2d a)
+{
+    return a.x * a.x + a.y * a.y;
+}
+
+
+float dot(Vec2d a, Vec2d b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+
+PhysicsCircle::PhysicsCircle(Vec2d init_p_vec, Vec2d init_v_vec, float radius, float bounciness, float mass, bool global, Environment e, SDL_Color rgb)
+{
+    m_pos = init_p_vec;
+    m_vel = init_v_vec;
+    m_radius = radius;
+    m_bounciness = bounciness;
+    m_mass = mass;
+    m_global = global;
+    m_env = e;
+    m_col = rgb;
+    if (m_global)
+    {
+        m_force = m_env.global_force;
+    }
+}
+
+
+void PhysicsCircle::update(float dt)
+{
+    // --- Boundary collision ---
+    if (m_pos.x - m_radius <= m_env.left)
+    {
+        m_pos.x = m_env.left + m_radius;
+        m_vel.x *= -m_bounciness;
+    }
+    else if (m_pos.x + m_radius >= m_env.right)
+    {
+        m_pos.x = m_env.right - m_radius;
+        m_vel.x *= -m_bounciness;
+    }
+    if (m_pos.y - m_radius <= m_env.top)
+    {
+        m_pos.y = m_env.top + m_radius;
+        m_vel.y *= -m_bounciness;
+    }
+    else if (m_pos.y + m_radius >= m_env.bot)
+    {
+        m_pos.y = m_env.bot - m_radius;
+        m_vel.y *= -m_bounciness;
+    }
+    
+
+    m_acc.x += m_force.x / m_mass * dt;
+    m_acc.y += m_force.y / m_mass * dt;
+
+    m_vel.x += m_acc.x * dt;
+    m_vel.y += m_acc.y * dt;
+    
+    // --- Move ---
+    m_pos.x += m_vel.x * dt;
+    m_pos.y += m_vel.y * dt;
+    
+}
+
+
+void PhysicsCircle::render(SDL_Renderer* renderer)
+{
+    SDL_SetRenderDrawColor(renderer, m_col.r, m_col.g, m_col.b, m_col.a);
+    draw_circle(renderer, m_pos.x, m_pos.y, m_radius, CIRCLE_RESOLUTION);
+}
+
+
+CircleEngine::CircleEngine(std::vector<PhysicsCircle*> circles)
+{
+    m_circles = circles;
+}
+
+
+bool CircleEngine::_check_collide(PhysicsCircle c1, PhysicsCircle c2)
+{
+    return (sqrtf((c2.m_pos.x - c1.m_pos.x) * (c2.m_pos.x - c1.m_pos.x) + (c2.m_pos.y - c1.m_pos.y) * (c2.m_pos.y - c1.m_pos.y)) < c1.m_radius + c2.m_radius);
+}
+
+
+void CircleEngine::_resolve_static(PhysicsCircle* c1, PhysicsCircle* c2)
+{
+    float fDist = sqrtf((c2->m_pos.x - c1->m_pos.x) * (c2->m_pos.x - c1->m_pos.x) + (c2->m_pos.y - c1->m_pos.y) * (c2->m_pos.y - c1->m_pos.y));
+    float fOverlap = 0.5f * (fDist - c1->m_radius - c2->m_radius);
+   
+    c1->m_pos.x -= fOverlap * (c1->m_pos.x - c2->m_pos.x) / fDist;
+    c1->m_pos.y -= fOverlap * (c1->m_pos.y - c2->m_pos.y) / fDist;
+
+    c2->m_pos.x += fOverlap * (c1->m_pos.x - c2->m_pos.x) / fDist;
+    c2->m_pos.y += fOverlap * (c1->m_pos.y - c2->m_pos.y) / fDist;
+   
+}
+
+
+void CircleEngine::_resolve_dynamic_simple(PhysicsCircle* c1, PhysicsCircle* c2)
+{
+    float tempVel;
+    tempVel = c1->m_vel.x;
+    c1->m_vel.x = c2->m_vel.x;
+    c2->m_vel.x = tempVel;
+  
+    tempVel = c1->m_vel.y;
+    c1->m_vel.y = c2->m_vel.y;
+    c2->m_vel.y = tempVel;
+}
+ 
+
+
+void CircleEngine::_resolve_dynamic_elastic(PhysicsCircle* c1, PhysicsCircle* c2)
+{
+    // https://en.wikipedia.org/wiki/Elastic_collision
+    // Angle-free representation
+
+    Vec2d c1VelUnchanged = c1->m_vel; // This bug took me like 2 days to find
+
+    Vec2d diff_factor = sub_v(c1->m_pos, c2->m_pos);
+    float mass_factor = 2.0f * c2->m_mass / (c1->m_mass + c2->m_mass);
+    float scalar_factor =  dot(sub_v(c1->m_vel, c2->m_vel), sub_v(c1->m_pos, c2->m_pos)) / mag_sqr(sub_v(c1->m_pos, c2->m_pos));
+    
+    c1->m_vel = sub_v(c1->m_vel, mul_s(mass_factor * scalar_factor, diff_factor));
+
+    diff_factor = sub_v(c2->m_pos, c1->m_pos);
+    mass_factor = 2.0f * c1->m_mass / (c1->m_mass + c2->m_mass);
+    scalar_factor = dot(sub_v(c2->m_vel, c1VelUnchanged), sub_v(c2->m_pos, c1->m_pos)) / mag_sqr(sub_v(c2->m_pos, c1->m_pos));
+    
+    c2->m_vel = sub_v(c2->m_vel, mul_s(mass_factor * scalar_factor, diff_factor));
+}                              
+
+
+
+void CircleEngine::phys_update(float dt)
+{
+    std::vector<std::pair<PhysicsCircle*, PhysicsCircle*>> collidingPairs;
+    for (int i = 0; i < m_circles.size(); i++)
+    {
+        for (int j = 0; j < m_circles.size(); j++)
+        {
+            if (i != j)
+            {
+                if (this->_check_collide(*m_circles[i], *m_circles[j]))
+                {
+                    this->_resolve_static(m_circles[i], m_circles[j]);
+                    collidingPairs.push_back({ m_circles[i], m_circles[j] });
+                }
+            }
+        }
+    }
+
+    for (auto p : collidingPairs)
+    {
+        this->_resolve_dynamic_elastic(p.second, p.first);
+    }
+
+    for (auto c : this->m_circles)
+    {
+        c->update(dt);
+    }
+
+}
+
+
+void CircleEngine::render(SDL_Renderer* renderer)
+{
+    for (auto c : this->m_circles)
+    {
+        c->render(renderer);
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <Windows.h>
+#include <SDL_syswm.h>
+
+#include "circle.h"
+
+
+const char* WINTT = "Circles";
+
+
+int main(int argc, char* argv[])
+{
+    int initSuccess = SDL_Init(SDL_INIT_VIDEO);
+
+    if (initSuccess != 0)
+    {
+        std::cout << "Init error: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window* hWin = SDL_CreateWindow(WINTT, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, SDL_WINDOW_FULLSCREEN);
+    if (hWin == NULL)
+    {
+        std::cout << "Window error: " << SDL_GetError() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer* hRend = SDL_CreateRenderer(hWin, -1, SDL_RENDERER_ACCELERATED);
+    if (hRend == NULL)
+    {
+        std::cout << "Renderer error: " << SDL_GetError() << std::endl;
+        if (hWin != NULL)
+        {
+            SDL_DestroyWindow(hWin);
+        }
+        SDL_Quit();
+    }
+
+    int WINWT;
+    int WINHT;
+
+    SDL_GetRendererOutputSize(hRend, &WINWT, &WINHT);
+
+    std::cout << "Resolution: (" << WINWT << ", " << WINHT << ')' << std::endl;
+
+    SDL_Event event;
+    bool quit = false;
+
+    Environment main_env;
+    main_env.bot = (float)WINHT;
+    main_env.top = 0.0f;
+    main_env.left = 0.0f;
+    main_env.right = (float)WINWT;
+    main_env.global_force = { 0.0f, 1000.0f };
+
+    SDL_Color circleCol = { 255, 255, 255 };
+    //float circleRad = 50.0f;
+
+    std::vector<PhysicsCircle*> circleVec;
+    //circleVec.push_back(new PhysicsCircle{ {0, 0}, {0,0}, 150.0f, 0, 1, false, main_env, SDL_Color {255, 0, 0} });
+
+    for (int _ = 0; _ < 500; _++)
+    {
+        PhysicsCircle* cPtr = new PhysicsCircle({ (float)(rand() % (WINWT - 10) + 10), (float)(rand() % (WINHT - 10) + 10) }, { 1000, 1000 }, 10, 0.5, 1, true, main_env, circleCol);
+        circleVec.push_back(cPtr);
+    }
+
+    CircleEngine engine(circleVec);
+
+    Uint32 endTime, startTime = SDL_GetTicks();
+    float deltaTime = 0.0f;
+
+    int mouseX, mouseY, lastMouseX = 0, lastMouseY = 0;
+
+    while (!quit)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+
+            case SDL_QUIT:
+                quit = true;
+                break;
+
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_ESCAPE:
+                    quit = true;
+                    break;
+                }
+
+                break;
+            }
+        }
+
+       /*
+        SDL_GetMouseState(&mouseX, &mouseY);
+
+        
+        engine.m_circles[0]->m_vel.x = (mouseX - lastMouseX) / deltaTime;
+        engine.m_circles[0]->m_vel.y = (mouseY - lastMouseY) / deltaTime;
+
+        engine.m_circles[0]->m_pos.x = mouseX;
+        engine.m_circles[0]->m_pos.y = mouseY;
+
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+        */
+        
+        //std::cout << engine.m_circles[1]->m_pos.x << ' ' << engine.m_circles[1]->m_pos.y << std::endl;
+
+        engine.phys_update(deltaTime);
+   
+        SDL_SetRenderDrawColor(hRend, 0, 0, 0, 255);
+        SDL_RenderClear(hRend);
+         
+        engine.render(hRend);
+
+        SDL_RenderPresent(hRend);
+
+        // Manage frame rate
+        endTime = SDL_GetTicks();
+
+        deltaTime = (endTime - startTime) / 1000.0f;
+        startTime = endTime;
+      
+
+        //std::cout << 1 / deltaTime << std::endl; // fps
+    }
+
+    SDL_DestroyRenderer(hRend);
+    SDL_DestroyWindow(hWin);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
